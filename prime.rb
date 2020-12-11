0b18cede54bcec345e669306b22676d4a35ffcee
Danny Brandt <dvbrandt90@gmail.com>
2020-12-11 14:40:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,14 +7,10 @@ if num  == 2 || num == 3
   true
 elsif num <= 0
   false
-elsif num > 0
-while i < num
- if num % i == 0 
+elsif num % i == 0 
    false 
    i = i + 1
  else
    true 
-   end
-  end
  end
 end
