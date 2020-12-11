d7d95a64bed6d7eaea09a1287913e2a8327f0dc9
Danny Brandt <dvbrandt90@gmail.com>
2020-12-11 14:36:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+
+
+def prime?(num)
+i = 3
+if num  == 2 || num == 3
+  puts 'its a 2  or 3'
+  true
+elsif num <= 0
+  false
+else
+while i < num
+ if num % i == 0 
+   false 
+   i = i + 1
+ else
+   true 
+   end
+  end
+ end
+end
