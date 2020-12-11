b29bd1763c7369b54c212f227eabf34bc8c72e56
Danny Brandt <dvbrandt90@gmail.com>
2020-12-11 14:49:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,7 @@
 
 def prime?(num)
 range = (2..num-1)
-if num  == 2 || num == 3
-  true
-elsif num <= 0
-  false
-elsif range.any? {|number| num % number == 0}
+if range.any? {|number| num % number == 0}
   false 
  else
    true 
